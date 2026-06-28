37eb63b9de2fddc80274a23a4ac7b7102cd0851f
Omar Eddari <eddari.omar.97@gmail.com>
2026-06-28 16:45:02 +0100

    mssg

---
+++ src/main/java/ma/enset/kafkastreamstext/StreamsConfig.java
@@ -0,0 +1,4 @@
+package ma.enset.kafkastreamstext;
+
+public class StreamsConfig {
+}
---
+++ src/test/java/ma/enset/kafkastreamstext/KafkaStreamsTextApplicationTests.java
@@ -0,0 +1,13 @@
+package ma.enset.kafkastreamstext;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class KafkaStreamsTextApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
